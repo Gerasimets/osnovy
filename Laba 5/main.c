480ba1a7dc6d47142f310ac6686d5ea13750f1c2
Gerasimets <47577104+Gerasimets@users.noreply.github.com>
2019-05-31 10:33:19 +0300

    Update main.c

--- Laba 5/main.c
+++ Laba 5/main.c
@@ -24,21 +24,3 @@ printf("\n");
 getch();
 return 0;
 }
- 
-#include <stdio.h>
-int main( void )
-{
-int L;
-int v2,res,Z;
-res=0;
-printf("L = ");
-scanf("%d", &L);
-while( L > 0)
-{
-res=res+1;
-Z=L%2;
-L=L/2;
-}
-printf("pos =%d",res);
-return 0;
-}
