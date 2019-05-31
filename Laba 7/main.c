04958856b9ba1698af07243dbbde0fad7b78faf4
Gerasimets <47577104+Gerasimets@users.noreply.github.com>
2019-05-31 10:32:36 +0300

    Create main.c

---
+++ Laba 7/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main( void )
+{
+int L;
+int v2,res,Z;
+res=0;
+printf("L = ");
+scanf("%d", &L);
+while( L > 0)
+{
+res=res+1;
+Z=L%2;
+L=L/2;
+}
+printf("pos =%d",res);
+return 0;
+}
