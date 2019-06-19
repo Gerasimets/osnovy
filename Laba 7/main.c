def8c581088b7e466f7b95778403a43570659207
Gerasimets <47577104+Gerasimets@users.noreply.github.com>
2019-06-19 03:37:00 +0300

    Update main.c

--- Laba 7/main.c
+++ Laba 7/main.c
@@ -1,17 +1,56 @@
+#pragma warning(disable : 4996)
 #include <stdio.h>
-int main( void )
+#define YES 1
+#define NO 0
+
+int main(void)
 {
-int L;
-int v2,res,Z;
-res=0;
-printf("L = ");
-scanf("%d", &L);
-while( L > 0)
-{
-res=res+1;
-Z=L%2;
-L=L/2;
-}
-printf("pos =%d",res);
-return 0;
+	long long int N; // длинное целое число
+	int res = 0; // результат (самая левая единица в двоичной системе)
+	int i;
+	int j;
+	int flag;
+	printf ("N = ");
+	scanf ("%lli", &N); // ввод длинного целого числа
+
+	/*вывод в двоичном виде*/
+	if (N >= 0)
+	{
+		for (j = 8; j > 0; --j)
+		{
+			for (i = 1; i < 5; ++i)
+			{
+				printf("%d", (N >> j * 4 - i & 1));
+			}
+			printf(" ");
+		}
+	}
+	else
+	{
+		for (j = 8, flag = YES; j > 0; --j)
+		{
+			for (i = 1; i < 5; ++i)
+			{
+				if (flag == YES)
+				{
+					flag = NO;
+					printf("1");
+				}
+				else
+				{
+					printf("%d", ((~N + 1) >> j * 4 - i & 1));
+				}
+			}
+			printf(" ");
+		}
+	}
+	printf("\n"); // конец вывода в двоичном виде
+
+	while (N > 0) // пока N не станет равно 0
+	{
+		res = res + 1; // увеличиваем позицию результата
+		N = N >> 1; // сдвигаем исходное число вправо
+	}
+	printf("pos = %d", res);
+	return 0;
 }
